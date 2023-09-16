5d53b9a52db3d807a488d32e0a219933ffcf9f81
EISHU92 <donatto555@gmail.com>
2023-09-16 13:18:20 -0400

    Make player team skin be used in other mods

--- mods/engine/bs_player_assignments/init.lua
+++ mods/engine/bs_player_assignments/init.lua
@@ -1,10 +1,16 @@
-if config.OverridePlayersSkinForTeams then
-	bs.cbs.register_OnAssignTeam(function(index, team)
+function SetTeamSkin(index, team)
+	if config.OverridePlayersSkinForTeams then
 		if team ~= "" then
 			local player = Player(index)
 			local textures = player:get_properties().textures
 			textures[1] = textures[1].."^player_"..team.."_overlay.png"
 			player:set_properties({textures = textures})
 		end
+	end
+end
+
+if config.OverridePlayersSkinForTeams then
+	bs.cbs.register_OnAssignTeam(function(index, team)
+		SetTeamSkin(index, team)
 	end)
 end
