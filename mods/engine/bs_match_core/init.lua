a92a781e4dfafcb60ab4af8e48180a756b84d535
EISHU92 <donatto555@gmail.com>
2023-09-16 11:09:11 -0400

    Respawn died players

---
+++ mods/engine/bs_match_core/init.lua
@@ -0,0 +1,11 @@
+if config.UseDefaultMatchEngine then
+	bs_match.register_SecondOnEndMatch(function()
+		core.after(0.5, function()
+			for team, data in pairs(bs.team) do
+				for name in pairs(data.players) do
+					bs.allocate_to_team(name, team, true)
+				end
+			end
+		end)
+	end)
+end
