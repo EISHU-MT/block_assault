408f072aaada4d49304eb10267a8f0c380b87923
EISHU92 <donatto555@gmail.com>
2023-09-25 22:25:14 -0400

    Fix crash

--- mods/engine/bs_player_assignments/init.lua
+++ mods/engine/bs_player_assignments/init.lua
@@ -2,9 +2,11 @@ function SetTeamSkin(index, team)
 	if config.OverridePlayersSkinForTeams then
 		if team ~= "" then
 			local player = Player(index)
-			local textures = player:get_properties().textures
-			textures[1] = textures[1].."^player_"..team.."_overlay.png"
-			player:set_properties({textures = textures})
+			if player:get_properties() then
+				local textures = player:get_properties().textures
+				textures[1] = textures[1].."^player_"..team.."_overlay.png"
+				player:set_properties({textures = textures})
+			end
 		end
 	end
 end
