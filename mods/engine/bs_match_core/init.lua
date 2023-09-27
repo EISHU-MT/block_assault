f8f9491209206a56963a5c864384c3ac97d26d40
EISHU92 <donatto555@gmail.com>
2023-09-26 21:09:16 -0400

    ReSpawn player at on match start

--- mods/engine/bs_match_core/init.lua
+++ mods/engine/bs_match_core/init.lua
@@ -1,17 +1,20 @@
-if config.UseDefaultMatchEngine then
-	bs_match.register_SecondOnEndMatch(function()
-		core.after(0.5, function()
-			for team, data in pairs(bs.team) do
-				for name in pairs(data.players) do
-					bs.allocate_to_team(name, "", true, true)
-					SpawnPlayerAtRandomPosition(Player(name), team)
-				end
+local function to_use()
+	core.after(0.5, function()
+		for team, data in pairs(bs.team) do
+			for name in pairs(data.players) do
+				bs.allocate_to_team(name, "", true, true)
+				SpawnPlayerAtRandomPosition(Player(name), team)
 			end
-		end)
-		core.after(0.7, function()
-			bs_timer.pause()
-			local id = annouce.publish_to_players("Prepare!", 0xFFFFF)
-			core.after(1.5, make_dissapear_mess, id)
-		end)
+		end
 	end)
+	core.after(0.7, function()
+		bs_timer.pause()
+		local id = annouce.publish_to_players("Prepare!", 0xFFFFF)
+		core.after(1.5, make_dissapear_mess, id)
+	end)
+end
+
+if config.UseDefaultMatchEngine then
+	bs_match.register_SecondOnEndMatch(to_use)
+	bs_match.register_OnMatchStart(to_use)
 end
