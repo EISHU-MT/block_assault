5acd52c9b6d80167c4552eed2c770eb6a0579640
EISHU92 <donatto555@gmail.com>
2023-09-27 18:39:17 -0400

    Add maps custom data

--- mods/engine/bs_maps/proccesor.lua
+++ mods/engine/bs_maps/proccesor.lua
@@ -24,7 +24,6 @@ function process_meta(content)
 		author = content.meta:get("author"),
 		offset = content.offset,
 		physics = core.deserialize(content.meta:get("physics")) or {jump = 1, speed = 1, gravity = 1},
-		status = core.deserialize(content.meta:get("status")),
 		teams = {
 			red = add(content.offset, core.string_to_pos(content.meta:get("team.red"))),
 			blue = add(content.offset, core.string_to_pos(content.meta:get("team.blue"))),
@@ -32,42 +31,12 @@ function process_meta(content)
 		failed = false,
 		pos1 = vector.add(content.offset, { x = -r, y = -h / 2, z = -r }),
 		pos2 = vector.add(content.offset, { x = r, y = h / 2, z = r }),
-		area_status = {},
+		data = core.deserialize(content.meta:get("data")), -- Data for mods/game. Used from external overriders to determine map.
 	}
 	
 	if content.meta:get("team.yellow") and content.meta:get("team.green") then
 		map.teams.yellow = add(content.offset, core.string_to_pos(content.meta:get("team.yellow")))
 		map.teams.green = add(content.offset, core.string_to_pos(content.meta:get("team.green")))
-	end
-	
-	if type(map.status) == "table" then
-		for name, value in pairs(map.status) do
-			if name and type(value) == "table" and value.pos and type(value.pos) == "table" and value.str then
-				core.log("action", "Registering "..name.." for Area names....")
-				if type(map.area_status[name]) ~= "table" then
-					map.area_status[name] = {}
-				end
-				local pos1 = add(content.offset, core.string_to_pos(value.pos.p1))
-				local pos2 = add(content.offset, core.string_to_pos(value.pos.p2))
-				
-				if pos1.y > pos2.y then
-					up_pos = pos1
-					down_pos = pos2
-				elseif pos2.y > pos1.y then
-					up_pos = pos2
-					down_pos = pos1
-				elseif pos2.y == pos1.y then
-					up_pos = pos1
-					down_pos = pos2
-				elseif not pos2 or not pos1 then
-					core.log("BsMaps engine Failed!: On searching non_radius areas: attempt to get a non existent value!: (pos1 and pos2)")
-				end
-				
-				map.area_status[name].pos1 = up_pos
-				map.area_status[name].pos2 = down_pos
-				map.area_status[name].str = value.str
-			end
-		end
 	end
 	return map
 end
