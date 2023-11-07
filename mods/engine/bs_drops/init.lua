96a5af17d4dc455e3f10832b440a03e856ec200a
EISHU92 <donatto555@gmail.com>
2023-11-07 18:08:20 -0500

    Add players drop inventory

---
+++ mods/engine/bs_drops/init.lua
@@ -0,0 +1,20 @@
+dropondie = {}
+
+local function drop_list(pos, inv, list)
+	for _, item in ipairs(inv:get_list(list)) do
+		local obj = minetest.add_item(pos, item)
+		if obj then
+			obj:set_velocity({ x = math.random(-1, 1), y = 5, z = math.random(-1, 1) })
+		end
+	end
+	inv:set_list(list, {})
+end
+
+function dropondie.drop_all(player)
+	local pos = player:get_pos()
+	pos.y = math.floor(pos.y + 0.5)
+	drop_list(pos, player:get_inventory(), "main")
+end
+
+minetest.register_on_dieplayer(dropondie.drop_all)
+minetest.register_on_leaveplayer(dropondie.drop_all)
