af86da3637af9a3044277a532c822264b7fe92e3
EISHU92 <donatto555@gmail.com>
2023-11-08 11:24:08 -0500

    Add "Town Map" to Engine

---
+++ mods/engine/town/init.lua
@@ -0,0 +1,12 @@
+--[[
+
+	This inst a traditional mod!, This is a map for BlockAssault Game
+	Dont modify this until you know what you doing.
+	BAM.S. (BlockAssaultMapmaker . System)
+--]]
+local modpath = core.get_modpath(core.get_current_modname())
+maps.register_map("town", {
+	name = "A town",
+	dirname = modpath,
+})
+--- END OF GEN. FILE ---
