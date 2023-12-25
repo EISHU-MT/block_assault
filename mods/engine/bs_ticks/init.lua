5d648f9de847ab860f9d0cad2167df5a8fe3d8b1
EISHU92 <donatto555@gmail.com>
2023-12-25 01:07:16 -0500

    Add cooldowns

--- mods/engine/bs_ticks/init.lua
+++ mods/engine/bs_ticks/init.lua
@@ -6,12 +6,19 @@ steps = {
 	state = true,
 	funcs = {},
 	old_register = core.register_globalstep,
+	cooldowns = {},
 }
 
 core.register_globalstep(function(dtime)
 	if steps.state then
 		for _, func in pairs(steps.funcs) do
 			func(dtime)
+		end
+	end
+	for id, sec in pairs(steps.cooldowns) do
+		steps.cooldowns[id] = steps.cooldowns[id] - dtime
+		if steps.cooldowns[id] <= 0 then
+			steps.cooldowns[id] = nil
 		end
 	end
 end)
@@ -32,8 +39,23 @@ end
 
 
 
-
-
+function steps.register_cooldown()
+	return {
+		set = function(self, time_sec, id)
+			steps.cooldowns[id] = time_sec
+		end,
+		is_zero = function(self, id)
+			if not steps.cooldowns[self.id] then
+				return true
+			end
+		end,
+		get_secs = function(self, id)
+			if steps.cooldowns[id] then
+				return steps.cooldowns[id]
+			end
+		end,
+	}
+end
 
 
 
