d94369f7c928d06671b39f04397701c5bdb312c5
EISHU92 <donatto555@gmail.com>
2024-02-12 18:51:32 -0300

    Move kill list & Fix bugs

--- mods/engine/bs_kill_history/init.lua
+++ mods/engine/bs_kill_history/init.lua
@@ -12,27 +12,27 @@ local KILLSTAT_REMOVAL_TIME = 30
 
 local MAX_NAME_LENGTH = 19
 local HUD_LINES = 6
-local HUD_LINE_HEIGHT = 36
+local HUD_LINE_HEIGHT = 20
 local HUDNAME_FORMAT = "kill_list:%d,%d"
 
 local HUD_DEFINITIONS = {
 	{
 		hud_elem_type = "text",
-		position = {x = 0, y = 0.8},
+		position = {x = 0.6, y = 0.3},
 		offset = {x = MAX_NAME_LENGTH*10, y = 0},
 		alignment = {x = "left", y = "center"},
 		color = 0xFFF,
 	},
 	{
 		hud_elem_type = "image",
-		position = {x = 0, y = 0.8},
-		image_scale = 2,
+		position = {x = 0.6, y = 0.3},
+		image_scale = 1.5,
 		offset = {x = (MAX_NAME_LENGTH*10) + 28, y = 0},
 		alignment = {x = "center", y = "center"},
 	},
 	{
 		hud_elem_type = "text",
-		position = {x = 0, y = 0.8},
+		position = {x = 0.6, y = 0.3},
 		offset = {x = (MAX_NAME_LENGTH*10) + 54, y = 0},
 		alignment = {x = "right", y = "center"},
 		color = 0xFFF,
@@ -44,28 +44,42 @@ local kill_list = {}
 local function update_hud_line(player, idx, new)
 	idx = HUD_LINES - (idx-1)
 
-	for i=1, 3, 1 do
+	for i=1, 4, 1 do
 		local hname = string.format(HUDNAME_FORMAT, idx, i)
 		local phud = hud:get(player, hname)
 
 		if new then
 			if phud then
-				hud:change(player, hname, {
-					text = (new[i].text or new[i] or ""),
-					color = new[i].color or 0xFFF
-				})
+				if i <= 3 then
+					hud:change(player, hname, {
+						text = (new[i].text or new[i] or ""),
+						color = new[i].color or 0xFFF
+					})
+				else
+					if i == 4 then
+						hud:change(player, hname, {
+							--text = (new[i].text or new[i] or ""),
+							--color = new[i].color or 0xFFF
+							image_scale = new[i]
+						})
+					end
+					print("took")
+				end
 			else
-				local newhud = table.copy(HUD_DEFINITIONS[i])
-
-				newhud.offset.y = -(idx-1)*HUD_LINE_HEIGHT
-				newhud.text = new[i].text or new[i]
-				newhud.color = new[i].color or 0xFFF
-				hud:add(player, hname, newhud)
+				if i <= 3 then
+					local newhud = table.copy(HUD_DEFINITIONS[i])
+					newhud.offset.y = -(idx-1)*HUD_LINE_HEIGHT
+					newhud.text = new[i].text or new[i]
+					newhud.color = new[i].color or 0xFFF
+					hud:add(player, hname, newhud)
+				end
 			end
 		elseif phud then
-			hud:change(player, hname, {
-				text = ""
-			})
+			--if i <= 3 then
+				hud:change(player, hname, {
+					text = ""
+				})
+			--end
 		end
 	end
 end
@@ -77,8 +91,8 @@ local function update_kill_list_hud(player)
 end
 
 local globalstep_timer = 0
-function KillHistory.RawAdd(x, y, z)
-	table.insert(kill_list, 1, {x, y, z})
+function KillHistory.RawAdd(x, y, z, s)
+	table.insert(kill_list, 1, {x, y, z, s})
 
 	if #kill_list > HUD_LINES then
 		table.remove(kill_list)
@@ -114,11 +128,13 @@ minetest.register_on_joinplayer(function(player)
 	update_kill_list_hud(player)
 end)
 
-function KillHistory.add(killer, victim, weapon_image, comment, color)
+function KillHistory.add(killer, victim, weapon_image, comment, color, imgS)
+	local s = imgS or 1.5
 	KillHistory.RawAdd(
 		{text = killer, color = color or bs.get_team_color(bs.get_team(killer), "number")},
 		weapon_image or "hand_kill.png",
-		{text = victim .. (comment or ""), color = bs.get_team_color(bs.get_team(victim), "number") or 0xFFF}
+		{text = victim .. (comment or ""), color = bs.get_team_color(bs.get_team(victim), "number") or 0xFFF},
+		s
 	)
 end
 
@@ -130,6 +146,7 @@ PvpCallbacks.RegisterFunction(function(data)
 	local image = "hand_kill.png"
 	local killer_name = ""
 	local color
+	local s
 	if type(data.killer) == "string" then -- Think that was a suicide attempt
 		if data.killer == "fall" then
 			image = "fall.png"
@@ -151,7 +168,7 @@ PvpCallbacks.RegisterFunction(function(data)
 	elseif type(data.killer) == "userdata" then
 		-- Extreme code beggining
 		if data.killer:is_player() then
-			local hand_item = data.killer:get_wielded_item()
+			local hand_item = bs.latest_used_item[Name(data.killer)] or data.killer:get_wielded_item()
 			local desc = hand_item:get_definition()
 			if desc.RW_gun_capabilities then
 				image = desc.RW_gun_capabilities.gun_icon.."^[transformFX"
@@ -160,6 +177,7 @@ PvpCallbacks.RegisterFunction(function(data)
 					image = desc.inventory_image
 				end
 			end
+			s = 1
 			killer_name = Name(data.killer)
 		else
 			local ent = data.killer:get_luaentity()
@@ -173,9 +191,10 @@ PvpCallbacks.RegisterFunction(function(data)
 						image = desc.inventory_image
 					end
 				end
+				s = 1
 				killer_name = ent.bot_name
 			end
 		end
 	end
-	KillHistory.add(killer_name, Name(data.died), image, "", color)
+	KillHistory.add(killer_name, Name(data.died), image, "", color, s)
 end, "BA.S Kill History System")
