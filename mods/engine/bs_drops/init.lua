e8029b7f9b245c49868724314eee87957d3862ca
Samuel Sargent <90115230+Python-Sargent@users.noreply.github.com>
2024-08-06 09:25:42 -0400

    Update init.lua
    make spectators not pick up items.

--- mods/engine/bs_drops/init.lua
+++ mods/engine/bs_drops/init.lua
@@ -46,3 +46,16 @@ core.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool
 end)
 
 minetest.register_on_leaveplayer(dropondie.drop_all)
+
+--[[
+if bs.spectator[Name(hitter)] or bs.spectator[Name(player)] then -- Dont allow spectators do damage.
+		return true
+	end
+]]--
+
+minetest.register_on_item_pickup(
+	function(itemstack, picker, pointed_thing, time_from_last_punch)
+		if bs.spectator[Name(picker)] or bs.spectator[Name(picker)] then
+			return Itemstack("")
+		end
+	end)
