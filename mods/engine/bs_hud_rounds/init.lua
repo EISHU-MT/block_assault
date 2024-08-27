27deba9a13dc3dfbc30eeaa127ede9c22b18554d
-NiSep9- <donatto555@gmail.com>
2024-08-26 22:28:46 -0500

    Move rounds hud

--- mods/engine/bs_hud_rounds/init.lua
+++ mods/engine/bs_hud_rounds/init.lua
@@ -3,93 +3,58 @@ bshud = {
 	["blue"] = 0,
 	["yellow"] = 0,
 	["green"] = 0,
+	["rounds"] = {}
 }
-rhud_round = {}
-bhud_round = {}
-yhud_round = {}
-ghud_round = {}
-minetest.register_on_joinplayer(function(ObjectRef, last_login)
-	local pname = Name(ObjectRef)
-	ghud_round[pname] = ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.53, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.5},
-		alignment = {x = "center", y = "up"},
-		text = tostring(bshud.green),
-		number = bs.get_team_color("green", "number"),
-	})
+local CC = core.colorize
+local function get(team) return tostring(bshud[team]) end
+local function o(guidanse) return 20 + guidanse end
+local function gec(team) return bs.get_team_color(team, "number") end
+core.register_on_joinplayer(function(ObjectRef, last_login) -- bs.get_team_color("green", "number")
 	ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.52, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.8},
+		hud_elem_type = "image",
+		position = {x = 0, y = 1},
+		text = "hud_rounds_hud.png",
 		alignment = {x = "center", y = "up"},
-		text = "\\",
-		number = 0x000000,
+		offset = {x=30,y=-80},
+		scale = {x = 1.7, y = 0.8},
 	})
-	bhud_round[pname] = ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.51, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.5},
-		alignment = {x = "center", y = "up"},
-		text = tostring(bshud.blue),
-		number = bs.get_team_color("blue", "number"),
-	})
-	ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.5, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.8},
-		alignment = {x = "center", y = "up"},
-		text = "|",
-		number = 0x000000,
-	})
-	rhud_round[pname] = ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.49, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.5},
-		alignment = {x = "center", y = "up"},
-		text = tostring(bshud.red),
-		number = bs.get_team_color("red", "number"),
-	})
-	ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.48, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.8},
-		alignment = {x = "center", y = "up"},
-		text = "/",
-		number = 0x000000,
-	})
-	yhud_round[pname] = ObjectRef:hud_add({
-		hud_elem_type = "text",
-		name = "hud_rounds",
-		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.47, y = 0.040},
-		offset = {x = 0, y = 20},
-		size = {x = 1.5},
-		alignment = {x = "center", y = "up"},
-		text = tostring(bshud.yellow),
-		number = bs.get_team_color("yellow", "number"),
-	})
+	bshud.rounds[Name(ObjectRef)] = {
+		red = ObjectRef:hud_add({
+			hud_elem_type = "text",
+			position = {x=0,y=1},
+			offset = {x = o(0), y = -80},
+			alignment = {x = "center", y = "up"},
+			text = get("red"),
+			number = gec("red")
+		}),
+		blue = ObjectRef:hud_add({
+			hud_elem_type = "text",
+			position = {x=0,y=1},
+			offset = {x = o(20), y = -80},
+			alignment = {x = "center", y = "up"},
+			text = get("blue"),
+			number = gec("blue")
+		}),
+		yellow = ObjectRef:hud_add({
+			hud_elem_type = "text",
+			position = {x=0,y=1},
+			offset = {x = o(40), y = -80},
+			alignment = {x = "center", y = "up"},
+			text = get("yellow"),
+			number = gec("yellow")
+		}),
+		green = ObjectRef:hud_add({
+			hud_elem_type = "text",
+			position = {x=0,y=1},
+			offset = {x = o(60), y = -80},
+			alignment = {x = "center", y = "up"},
+			text = get("green"),
+			number = gec("green")
+		}),
+	}
 end)
 
-bs_match.register_OnEndMatch(function(team)
+function UpdateRoundsHud(team)
 	if team == "blue" then
 		bshud.blue = bshud.blue + 1
 	elseif team == "red" then
@@ -100,15 +65,11 @@ bs_match.register_OnEndMatch(function(team)
 		bshud.red = bshud.green + 1
 	end
 	for _, player in pairs(core.get_connected_players()) do
-		local pname = player:get_player_name()
-		if pname then
-			if rhud_round[pname] and bhud_round[pname] then
-				player:hud_change(bhud_round[pname], "text", tostring(bshud.blue))
-				player:hud_change(rhud_round[pname], "text", tostring(bshud.red))
-			end
+		for teamm, id in pairs(bshud.rounds[Name(player)]) do
+			player:hud_change(id, "text", tostring(get(teamm)))
 		end
 	end
-end)
+end
 
 bs_match.register_OnNewMatches(function()
 	bshud.blue = 0
