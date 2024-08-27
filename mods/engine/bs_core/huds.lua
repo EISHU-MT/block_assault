459929c7e0f34d15bc3cbaea09c67a7f0a05f1f2
-NiSep9- <donatto555@gmail.com>
2024-08-26 22:27:46 -0500

    Remove not used anymore: hud timer

--- mods/engine/bs_core/huds.lua
+++ mods/engine/bs_core/huds.lua
@@ -1,22 +1,22 @@
 minetest.register_on_joinplayer(function(ObjectRef, last_login)
 	local pname = Name(ObjectRef)
-	ObjectRef:hud_add({
-		hud_elem_type = "image",
-		scale = {x = 1.4, y = 3.1},
-		position = {x = 0.5, y = 0},
-		offset = {x = 0, y = 30},
-		--size = {x = 2},
-		alignment = {x = "center", y = "up"},
-		--alignment = {x = 0, y = -1},
-		text = "up_hud.png",
-		--number = 0xCECECE,
-	})
+	--ObjectRef:hud_add({
+	--	hud_elem_type = "image",
+	--	scale = {x = 1.4, y = 3.1},
+	--	position = {x = 0.5, y = 0},
+	--	offset = {x = 0, y = 30},
+	--	--size = {x = 2},
+	--	alignment = {x = "center", y = "up"},
+	--	--alignment = {x = 0, y = -1},
+	--	text = "up_hud.png",
+	--	--number = 0xCECECE,
+	--})
 	timehud[pname] = ObjectRef:hud_add({
 		hud_elem_type = "text",
 		name = "n",
 		scale = {x = 1.5, y = 1.5},
-		position = {x = 0.5, y = 0},
-		offset = {x = 0, y = 20},
+		position = {x = 0.5, y = 1},
+		offset = {x = 0, y = -80},
 		size = {x = 2},
 		alignment = {x = "center", y = "up"},
 		--alignment = {x = 0, y = -1},
