1cb827458493d6c609bb15d2fbdec73d1cbcd2eb
-NiSep9- <donatto555@gmail.com>
2024-09-01 00:15:29 -0500

    Dont generate excessive lag at map outbounds

--- mods/engine/bs_outbounds/init.lua
+++ mods/engine/bs_outbounds/init.lua
@@ -1,10 +1,9 @@
 local function func(dt)
 	if maps.current_map and maps.current_map.pos1 and maps.current_map.pos2 then
 		if bs_match.match_is_started then
-			local area = VoxelArea:new({MinEdge = maps.current_map.pos1, MaxEdge = maps.current_map.pos2})
 			for _, player in pairs(core.get_connected_players()) do
 				if player:get_pos() then
-					if not area:containsp(player:get_pos()) then
+					if not maps.is_on_interior(player:get_pos(), maps.current_map.pos1, maps.current_map.pos2) then--if not area:containsp(player:get_pos()) then
 						if (not bs.spectator[Name(player)]) or (not bs.player_team[Name(player)]) then
 							if maps.current_map.teams[bs.player_team[Name(player)]] then
 								player:set_pos(maps.current_map.teams[bs.player_team[Name(player)]])
