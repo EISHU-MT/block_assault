4d0e7fd1b310c65b855f6faeb1e0befdfb0e7b08
-NiSep9- <donatto555@gmail.com>
2024-09-29 16:03:07 -0500

    Update maps api

--- mods/engine/bs_maps/api.lua
+++ mods/engine/bs_maps/api.lua
@@ -2,9 +2,13 @@ function maps.register_map(map_name, def)
 	if map_name and def then
 		local data = table.copy(def)
 		data.meta = Settings(def.dirname.."/map.cfg")
+		data.technical_name = map_name
 		if not def.name then
 			data.name = map_name
 		end
+		--if (not def.map_texture) or (not def.image) then
+		data.image = def.map_texture or def.image or "no_map.png"
+		--end
 		local map = process_meta(data)
 		
 		if map.failed ~= true then
