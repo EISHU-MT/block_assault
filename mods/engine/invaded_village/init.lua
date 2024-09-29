1f01f4226b7bc3d78419c6a8faed5bbb64a883d3
-NiSep9- <donatto555@gmail.com>
2024-09-29 13:28:45 -0500

    Add texture image for map: Invaded Village

--- mods/engine/invaded_village/init.lua
+++ mods/engine/invaded_village/init.lua
@@ -7,6 +7,7 @@
 local modpath = core.get_modpath(core.get_current_modname())
 maps.register_map("invaded_village", {
 	name = "Invaded Village",
+	image = "INVADED_VILLAGES_MAP_TEX.png",
 	dirname = modpath,
 })
 --- END OF GEN. FILE ---
