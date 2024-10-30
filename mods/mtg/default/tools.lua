a6c642acdf04d18b173c1be35e6e1a1708aae7d0
-NiSep9- <donatto555@gmail.com>
2024-10-30 09:36:47 -0500

    Add new type of digging

--- mods/mtg/default/tools.lua
+++ mods/mtg/default/tools.lua
@@ -13,6 +13,7 @@ minetest.override_item("", {
 		groupcaps = {
 			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
 			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			choppy = {times={[2]=7.00, [3]=5.50, [4]=2, [5]=1}, uses=0, maxlevel=1},
 			oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
 		},
 		damage_groups = {fleshy=1},
