ec2cb961686405a8f1e30c8684cfcd20a0f16df8
-NiSep9- <donatto555@gmail.com>
2024-11-02 10:11:29 -0500

    Add little town map

---
+++ mods/maps/littletown/init.lua
@@ -0,0 +1,13 @@
+--[[
+
+	This inst a traditional mod!, This is a map for BlockAssault Game
+	Dont modify this until you know what you doing.
+	BAM.S. (BlockAssaultMapmaker . System)
+--]]
+local modpath = core.get_modpath(core.get_current_modname())
+maps.register_map("littletown", {
+	name = "Little Town",
+	dirname = modpath,
+	image = "littletown_show.png",
+})
+--- END OF GEN. FILE ---
